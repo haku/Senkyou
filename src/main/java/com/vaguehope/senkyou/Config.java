3d5703e14ebccded6431350f28922d7b330dcf50
はく <haku@vaguehope.com>
2012-01-22 17:37:47 +0000

    100% sonar compliance.

--- src/main/java/com/vaguehope/senkyou/Config.java
+++ src/main/java/com/vaguehope/senkyou/Config.java
@@ -2,13 +2,13 @@ package com.vaguehope.senkyou;
 
 public interface Config {
 	
-	final int USER_COUNT_MAX = 100;
-	final int USER_AGE_MAX = 10; // 10 minutes.
+	int USER_COUNT_MAX = 100;
+	int USER_AGE_MAX = 10; // 10 minutes.
 	
-	final int TWEET_FETCH_PAGE_SIZE = 40;
+	int TWEET_FETCH_PAGE_SIZE = 40;
 	
-	final long MAX_HOME_TIMELINE_AGE = 30000; // 30 seconds.
-	final int USER_TWEET_CACHE_COUNT_MAX = 100;
-	final int USER_TWEET_CACHE_AGE_MAX = 60; // 60 minutes.
+	long MAX_HOME_TIMELINE_AGE = 30000; // 30 seconds.
+	int USER_TWEET_CACHE_COUNT_MAX = 100;
+	int USER_TWEET_CACHE_AGE_MAX = 60; // 60 minutes.
 	
 }
