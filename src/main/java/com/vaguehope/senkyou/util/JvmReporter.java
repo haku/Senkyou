6a23a356bd56aebf7438bd9d236fe5ca594a7a3c
はく <haku@vaguehope.com>
2012-02-03 21:18:44 +0000

    Refactor to make Sonar happy.

--- src/main/java/com/vaguehope/senkyou/util/JvmReporter.java
+++ src/main/java/com/vaguehope/senkyou/util/JvmReporter.java
@@ -8,6 +8,7 @@ public class JvmReporter {
 
 	private static final long DELAY = 10L * 1000L; // 10 seconds.
 	private static final long INTERVAL = 5L * 60L * 1000L; // 5 minutes.
+	private static final long BYTES_IN_MB = 1024L * 1024L;
 
 	protected static final Logger LOG = Logger.getLogger(JvmReporter.class.getName());
 
@@ -21,20 +22,24 @@ public class JvmReporter {
 		this.timer.cancel();
 	}
 
+	protected static void logStats () {
+		StringBuilder s = new StringBuilder("JVM heap: ");
+		long heapFreeSize = Runtime.getRuntime().freeMemory() / BYTES_IN_MB;
+		long heapSize = Runtime.getRuntime().totalMemory() / BYTES_IN_MB;
+		s.append(heapSize - heapFreeSize);
+		s.append(" mb of ");
+		s.append(heapSize);
+		s.append(" mb used.");
+		LOG.info(s.toString());
+	}
+
 	private static class Task extends TimerTask {
 
 		public Task () {}
 
 		@Override
 		public void run () {
-			StringBuilder s = new StringBuilder("JVM heap: ");
-			long heapFreeSize = Runtime.getRuntime().freeMemory() / 1024 / 1024;
-			long heapSize = Runtime.getRuntime().totalMemory() / 1024 / 1024;
-			s.append(heapSize - heapFreeSize);
-			s.append(" mb of ");
-			s.append(heapSize);
-			s.append(" mb used.");
-			LOG.info(s.toString());
+			logStats();
 		}
 
 	}
