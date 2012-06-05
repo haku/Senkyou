857337c51c1c121a06078cc30a4c8ca76458bf05
はく <haku@vaguehope.com>
2012-06-05 14:47:00 +0100

    Whitespace

--- src/main/java/com/vaguehope/senkyou/model/TweetList.java
+++ src/main/java/com/vaguehope/senkyou/model/TweetList.java
@@ -17,49 +17,49 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlAccessorType(XmlAccessType.NONE)
 public class TweetList {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
+
 	@XmlAttribute(name = "time") private volatile long time;
 	@XmlElement(name = "tweet") private volatile Set<Tweet> tweets = Collections.synchronizedSet(new LinkedHashSet<Tweet>());
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
+
 	public TweetList () {
 		this.time = System.currentTimeMillis();
 	}
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
+
 	public long getTime () {
 		return this.time;
 	}
-	
+
 	public int tweetCount () {
 		return this.tweets.size();
 	}
-	
+
 	public Set<Tweet> getTweets () {
 		return Collections.unmodifiableSet(this.tweets);
 	}
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
+
 	public void setTime (long time) {
 		this.time = time;
 	}
-	
+
 	public void addTweet (Tweet t) {
 		this.tweets.add(t);
 	}
-	
+
 	public void addAdd (Collection<Tweet> t) {
 		this.tweets.addAll(t);
 	}
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
+
 	public void toXml (PrintWriter writer) throws JAXBException {
 		Model.getMarshaller().marshal(this, writer);
 	}
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 }
