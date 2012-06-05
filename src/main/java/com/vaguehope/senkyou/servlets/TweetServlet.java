45724ee1741595bf3ada5d8a78793ecce1d200be
はく <haku@vaguehope.com>
2012-06-05 17:14:37 +0100

    Actually send tweet.

--- src/main/java/com/vaguehope/senkyou/servlets/TweetServlet.java
+++ src/main/java/com/vaguehope/senkyou/servlets/TweetServlet.java
@@ -1,14 +1,15 @@
 package com.vaguehope.senkyou.servlets;
 
 import java.io.IOException;
-import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import twitter4j.StatusUpdate;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 
 import com.vaguehope.senkyou.DataStore;
 
@@ -17,7 +18,6 @@ public class TweetServlet extends HttpServlet {
 	public static final String CONTEXT = "/tweet";
 
 	private static final long serialVersionUID = -5606626611510620577L;
-	private static final Logger LOG = Logger.getLogger(TweetServlet.class.getName());
 
 	private transient final DataStore dataStore;
 
@@ -30,10 +30,17 @@ public class TweetServlet extends HttpServlet {
 		Twitter twitter = AuthServlet.getSessionTwitterOrSetError(req, resp, this.dataStore);
 		if (twitter == null) return;
 
-		String replyId = req.getParameter("replyId");
+		long replyTo = Long.parseLong(req.getParameter("replyTo"));
 		String body = req.getParameter("body");
 
-		LOG.info("Tweet in reply to " + replyId + ": " + body);
+		try {
+			StatusUpdate status = new StatusUpdate(body);
+			status.setInReplyToStatusId(replyTo);
+			twitter.updateStatus(status);
+		}
+		catch (TwitterException e) {
+			throw new ServletException(e);
+		}
 	}
 
 }
